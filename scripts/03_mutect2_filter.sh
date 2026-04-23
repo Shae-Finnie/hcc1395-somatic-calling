581e062582e8246dd9907db881acdf081ac018ac
Seamus-Finnegen <shaethomasfinnie@gmail.com>
2026-04-23 10:45:11 -0700

    Removed old versions of 00 and 03

--- scripts/03_mutect2_filter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-gatk FilterMutectCalls \
-  -R data/refs/Homo_sapiens_assembly38.fasta \
-  -V results/mutect2/somatic.vcf.gz \
-  --stats results/mutect2/somatic.vcf.gz.stats \
-  -O results/mutect2/somatic.filtered.vcf.gz

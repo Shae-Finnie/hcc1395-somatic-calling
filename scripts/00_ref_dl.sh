516d89be6ac0f6bad8f2f0155b2ed93845524b92
Seamus-Finnegen <shaethomasfinnie@gmail.com>
2026-04-22 18:56:23 -0700

    Updated 00_ref_dl.sh and remove bai's from the repo

--- scripts/00_ref_dl.sh
+++ scripts/00_ref_dl.sh
@@ -12,9 +12,9 @@ RES_DIR="data/resources"
 echo "**************************"
 echo "downloading hg38 ref files"
 echo "**************************"
-wget -P "$REF_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta
-wget -P "$REF_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.fai
-wget -P "$REF_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.dict
+wget -nc -P "$REF_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta
+wget -nc -P "$REF_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.fai
+wget -nc -P "$REF_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/Homo_sapiens_assembly38.dict
 echo "**************************"
 echo "hg38 ref files downloaded to data/refs"
 echo "**************************"
@@ -23,9 +23,9 @@ echo "**************************"
 echo "**************************"
 echo "downloading gnmoAD common human germline variants ref"
 echo "**************************"
-wget -P "$RES_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/somatic-hg38/af-only-gnomad.hg38.vcf.gz
-wget -P "$RES_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/somatic-hg38/af-only-gnomad.hg38.vcf.gz.tbi
-wget -P "$RES_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/wgs_calling_regions.hg38.interval_list
+wget -nc -P "$RES_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/somatic-hg38/af-only-gnomad.hg38.vcf.gz
+wget -nc -P "$RES_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/somatic-hg38/af-only-gnomad.hg38.vcf.gz.tbi
+wget -nc -P "$RES_DIR" https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/wgs_calling_regions.hg38.interval_list
 echo "**************************"
 echo "gnmoAD refs downloaded to data/resources"
 echo "**************************"
